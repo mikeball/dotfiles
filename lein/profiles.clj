9783a7944d663f6d7475d08d95a6930d4a79d7f9
Mike Ball <michaelhball@gmail.com>
2012-11-04 12:53:53 -0800

    Cleanup old plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,5 @@
-{:user {:plugins [[lein-swank "1.4.4"]
-                  [lein-ring "0.7.5"]
-                  [lein-cljsbuild "0.1.9"]
-                  [lein-newnew "0.2.6"]
-                  [cljs-template "0.1.5"]
+{:user {:plugins [[lein-ring "0.7.5"]
+                  ;; [lein-cljsbuild "0.2.9"]
+                  ;; [lein-newnew "0.2.6"]
+                  ;; [cljs-template "0.1.5"]
                   [lein-clojars "0.9.1"]]}}
